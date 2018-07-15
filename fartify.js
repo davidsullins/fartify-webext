874af8bc82888e1030fa67074a4c80f26af9a5c0
David Sullins <david.sullins@gmail.com>
2018-07-15 07:28:42 -0500

    Initial commit
    * Converts slack apologies into farts
    * Toolbar button doesn't do anything yet
    * Might require reloading the page

---
+++ fartify.js
@@ -0,0 +1,52 @@
+
+document.body.style.border = "5px solid red";
+walk(document.body);
+
+function walk(node) 
+{
+	// I stole this function from here:
+	// http://is.gd/mwZp7E
+	
+	var child, next;
+
+	switch ( node.nodeType )  
+	{
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child ) 
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+        		if(node.parentElement.tagName.toLowerCase() != "script") {
+        	        	handleText(node);
+        		}
+			break;
+	}
+}
+
+function handleText(textNode) 
+{
+	var v = textNode.nodeValue;
+
+	v = v.replace(/\bsorry\b/g, "farting");
+	v = v.replace(/\bSorry\b/g, "Farting");
+	v = v.replace(/\bapologize to\b/g, "fart at");
+	v = v.replace(/\bapologize\b/g, "fart");
+	v = v.replace(/\bApologize\b/g, "Fart");
+	v = v.replace(/\bapologizes\b/g, "farts");
+	v = v.replace(/\bApologizes\b/g, "Farts");
+	v = v.replace(/\bapologized\b/g, "farted");
+	v = v.replace(/\bApologized\b/g, "Farted");
+	v = v.replace(/\bapologizing\b/g, "farting");
+	v = v.replace(/\bApologizing\b/g, "Farting");
+	
+	textNode.nodeValue = v;
+}
+
