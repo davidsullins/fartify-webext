f46b2f96da3daf715d5c45884d125df485700713
David Sullins <david.sullins@gmail.com>
2018-07-22 03:04:44 -0500

    More robustly handle slack text input
    Now I only get updates to sent messages, so I can still type "sorry" in
    a message box.
    
    Also check for things to change before trying to replace anything,
    because running the text replacement, because updating the text resets
    the cursor position in a text entry box

--- fartify.js
+++ fartify.js
@@ -1,21 +1,17 @@
-
 walk(document.body);
 
-function walk(node) 
-{
+function walk(node) {
 	// I stole this function from here:
 	// http://is.gd/mwZp7E
-	
+
 	var child, next;
 
-	switch ( node.nodeType )  
-	{
+	switch (node.nodeType) {
 		case 1:  // Element
 		case 9:  // Document
 		case 11: // Document fragment
 			child = node.firstChild;
-			while ( child ) 
-			{
+			while (child) {
 				next = child.nextSibling;
 				walk(child);
 				child = next;
@@ -23,33 +19,39 @@ function walk(node)
 			break;
 
 		case 3: // Text node
-        		if(node.parentElement.tagName.toLowerCase() != "script") {
-        	        	handleText(node);
-        		}
+			// tagName "p" for the text entry box in Slack
+			if (node.parentElement.tagName.toLowerCase() != "script"
+				&& node.parentElement.tagName.toLowerCase() != "p"
+			) {
+				handleText(node);
+			}
 			break;
 	}
 }
 
-function handleText(textNode) 
-{
+function handleText(textNode) {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bsorry\b/g, "farting");
-	v = v.replace(/\bSorry\b/g, "Farting");
-	v = v.replace(/\bapologize to\b/g, "fart at");
-	v = v.replace(/\bapologize\b/g, "fart");
-	v = v.replace(/\bApologize\b/g, "Fart");
-	v = v.replace(/\bapologizes\b/g, "farts");
-	v = v.replace(/\bApologizes\b/g, "Farts");
-	v = v.replace(/\bapologized\b/g, "farted");
-	v = v.replace(/\bApologized\b/g, "Farted");
-	v = v.replace(/\bapologizing\b/g, "farting");
-	v = v.replace(/\bApologizing\b/g, "Farting");
-	v = v.replace(/\bapology\b/g, "fart");
-	v = v.replace(/\bApology\b/g, "Fart");
-	v = v.replace(/\bapologies\b/g, "farts");
-	v = v.replace(/\bApologies\b/g, "Farts");
-	
-	textNode.nodeValue = v;
+	if (v.match(/orry\b/) || v.match(/polog/)) {
+		v = v.replace(/\bsorry\b/g, "farting");
+		v = v.replace(/\bSorry\b/g, "Farting");
+		v = v.replace(/\bapologize to\b/g, "fart at");
+		v = v.replace(/\bapologize\b/g, "fart");
+		v = v.replace(/\bApologize\b/g, "Fart");
+		v = v.replace(/\bapologizes\b/g, "farts");
+		v = v.replace(/\bApologizes\b/g, "Farts");
+		v = v.replace(/\bapologized\b/g, "farted");
+		v = v.replace(/\bApologized\b/g, "Farted");
+		v = v.replace(/\bapologizing\b/g, "farting");
+		v = v.replace(/\bApologizing\b/g, "Farting");
+		v = v.replace(/\bapology\b/g, "fart");
+		v = v.replace(/\bApology\b/g, "Fart");
+		v = v.replace(/\bapologies\b/g, "farts");
+		v = v.replace(/\bApologies\b/g, "Farts");
+		if (v != textNode.nodeValue) {
+			//console.log(textNode.parentElement.tagName);
+			textNode.nodeValue = v;
+		}
+	}
 }
 
